c41d68acd9f1780b05d6b905adc48d6ccb7951d5
David Lin <linwdav@gmail.com>
2015-09-17 00:12:04 -0700

    Reformat project to use gradle

--- src/main/java/curbside/CurbsideChallenge.java
+++ src/main/java/curbside/CurbsideChallenge.java
@@ -1,3 +1,5 @@
+package curbside;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 
--- src/main/java/curbside/CurbsideResponseObjectSpecial.java
+++ src/main/java/curbside/CurbsideResponseObjectSpecial.java
@@ -1,3 +1,5 @@
+package curbside;
+
 import java.util.ArrayList;
 import java.util.List;
 
